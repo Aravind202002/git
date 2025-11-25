7987d83b2142e395f7c4e73b407a0f7aa1b0efd1
aravind <aravind202002k@gmail.com>
2025-11-25 16:43:06 +0530

    GBU mamey

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("welcome here we are going to learn the git and github detail")
+console.log("welcome to world of GBU mamey ");
+for (var i=0;i<=5;i++){
+    console.log("GBU mamey");
+}
