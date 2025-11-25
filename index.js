9712ec5e3f380c7d0b69b455c4188c169f6501ab
aravind <aravind202002k@gmail.com>
2025-11-25 16:47:14 +0530

    mamey

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("welcome here we are going to learn the git and github detail")
+console.log("welcome here we are going to learn the git and github detail");
+for (var i=0;i<=5;i++){
+    console.log("helllo mamey");
+}
